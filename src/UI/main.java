3ae6c6855053af14f09df2efecc2be33dde886d1
steventhenelson <stnelson@pdx.edu>
2017-07-11 20:10:02 -0700

    Update main.java

--- src/UI/main.java
+++ src/UI/main.java
@@ -1,5 +1,5 @@
 public class main {
-    public static void main( String[] args ) {
+    public void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.exit( 0 ); //success
     }
