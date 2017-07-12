0b0dee1f490b275775274433dc2be95e0777b57b
steventhenelson <stnelson@pdx.edu>
2017-07-11 19:58:29 -0700

    Add files via upload
    Main file

---
+++ FTP.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Nelson on 7/11/2017.
+ */
+public class FTP {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.exit( 0 ); //success
+    }
+}
+
