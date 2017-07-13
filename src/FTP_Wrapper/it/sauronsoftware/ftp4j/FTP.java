c2d0a0055999c61a3a9b7bf99b9a3aa464b0eea7
steventhenelson <stnelson@pdx.edu>
2017-07-12 19:32:14 -0700

    Update FTP.java

--- src/FTP_Wrapper/it/sauronsoftware/ftp4j/FTP.java
+++ src/FTP_Wrapper/it/sauronsoftware/ftp4j/FTP.java
@@ -4,9 +4,7 @@ public class FTP {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         FTPClient client = new FTPClient();
-
-        FTPClient client = new FTPClient();
-
+        
         System.exit( 0 ); //success
     }
 }
