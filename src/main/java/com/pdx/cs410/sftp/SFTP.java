4e9ebf22657a97c7a0d9b24b43248552d98ef971
Noah Mcaulay <noah.mcaulay@gmail.com>
2017-08-07 21:08:50 -0700

    Added in saving and loading of connection info.

--- src/main/java/com/pdx/cs410/sftp/SFTP.java
+++ src/main/java/com/pdx/cs410/sftp/SFTP.java
@@ -1,8 +1,8 @@
-package com.pdx.cs410.sftp; /**
- * Created by Sean on 7/13/2017.
- */
+package com.pdx.cs410.sftp;
 
 import com.jcraft.jsch.*;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class SFTP {
@@ -15,6 +15,10 @@ public class SFTP {
     public static String username = null;
     public static String password = null;
     public static int PORT = 0;
+    public static String saveInfo = null;
+    public static String useSavedInfo = null;
+    public static boolean isUsingSavedInfo = false;
+
     public static void main(String[] args){
             JSch.setConfig("StrictHostKeyChecking", "no");
             if(login()){
@@ -23,19 +27,36 @@ public class SFTP {
             session.disconnect();
             return;
     }
+
     private static boolean login(){
         String answer;
         boolean lockDown = false;
         while(!lockDown){
-            System.out.print("Please enter a hostname: ");
-            hostname = in.nextLine();
-            System.out.print("Enter a port number: ");
-            PORT = in.nextInt();
-            in.nextLine();
-            System.out.print("Enter your username: ");
-            username = in.nextLine();
+
+            // check if a file with connection info exists
+            if (loadExistingConnectionInfo()) {
+                System.out.println("Would you like to use your saved connection info (" + username + "@" + hostname + ")? (y/n)");
+                useSavedInfo = in.nextLine();
+
+                // check if the user wants to use the saved info
+                if (useSavedInfo.toLowerCase().equals("y")) {
+                    isUsingSavedInfo = true;
+                }
+            }
+
+            if (!isUsingSavedInfo) {
+                System.out.print("Please enter a hostname: ");
+                hostname = in.nextLine();
+                System.out.print("Enter a port number: ");
+                PORT = in.nextInt();
+                in.nextLine();
+                System.out.print("Enter your username: ");
+                username = in.nextLine();
+            }
+
             System.out.print("Enter your password: ");
             password = in.nextLine();
+
             try{
                 //session = client.getSession("pater2", "linux.cs.pdx.edu", 22);
                 //session.setPassword("Kayle077r");
@@ -46,6 +67,23 @@ public class SFTP {
                 channel.connect();
                 channelSftp = (ChannelSftp) channel;
                 lockDown = true;
+
+                if (!isUsingSavedInfo) {
+                    // ask if user wants to save this new connection info
+                    System.out.println("Would you like to save your connection info? (y/n): ");
+                    saveInfo = in.nextLine();
+
+                    if (saveInfo.toLowerCase().equals("y")) {
+
+                        // save the info
+                        if (saveConnectionInfo()) {
+                            System.out.println("Your connection info (" + username + "@" + hostname + ") was saved successfully.");
+                        } else {
+                            System.out.println("ERROR: Your connection info was not saved.");
+                        }
+                    }
+                }
+
             }catch(JSchException e){
                 System.out.println("You were not logged into " + hostname + "due to a login error.");
                 System.out.print("Would you like to try again? (Y/N) ");
@@ -55,6 +93,69 @@ public class SFTP {
             }
         }
         return lockDown;
+    }
+
+    // saves the connection info (hostname, PORT, and username) to a file
+    // currently this will overwrite any existing file
+    private static boolean saveConnectionInfo() {
+
+        File file = new File("superSecureConnectionInfo.secret");
+
+        try {
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
+
+            // write connection info to the file
+            writer.write(hostname +"\n" + PORT + "\n" + username);
+            writer.flush();
+            writer.close();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    // checks if connection info already exists in a file, and if so it loads the info
+    private static boolean loadExistingConnectionInfo() {
+
+        File file = new File("superSecureConnectionInfo.secret");
+
+        if (file.exists()) {
+
+            // read the data from the file in a barbaric manner (probably needs a lot more error checking)
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line;
+
+                if ((line = br.readLine()) != null) { // disgusting
+                    hostname = line;
+                } else {
+                    return false;
+                }
+
+                if ((line = br.readLine()) != null) { // disgusting
+                    PORT = Integer.parseInt(line);
+                } else {
+                    return false;
+                }
+
+                if ((line = br.readLine()) != null) { // disgusting
+                    username = line;
+                } else {
+                    return false;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        return true;
     }
 
 }
